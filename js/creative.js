c24f74b0291ca314e976b2446f50f62025fe3f84
srelkins <35789181+srelkins@users.noreply.github.com>
2018-07-25 22:35:28 -0500

    attempted to add linke below project 14

--- js/creative.js
+++ js/creative.js
@@ -59,7 +59,7 @@
   // Magnific popup calls
   $('.popup-gallery').magnificPopup({
     delegate: 'a',
-    type: 'image',
+    type: 'inline',
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
     gallery: {
